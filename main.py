2281bcf261079f305b7fcdb2f595a843bfa56ea4
zOrg <mikhalev@zOrg.local>
2017-04-13 23:50:13 +0300

    minor

--- main.py
+++ main.py
@@ -33,37 +33,38 @@ def log(question, answer):
 # .start
 @bot.message_handler(commands=['start'])
 def handle_text(message):
-    bot.send_message(message.chat.id, "Вас приветствует магазин [Ёлки]({0})".format(const.logo), parse_mode="Markdown")
+    bot.send_message(message.chat.id, u"Вас приветствует магазин [Ёлки]({0})".format(const.logo), parse_mode="Markdown")
     bot.send_photo(message.chat.id, "AgADAgADDqgxG-osSEvKHpohZmjRgblMtw0ABGXOtqSQDsEYa_QBAAEC")
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
 # remove
-    user_markup.row('/start','/stop')
+#    user_markup.row('/start','/stop')
 # /remove
     user_markup.row(const.command_buy)
     user_markup.row(const.command_review, const.command_rules, const.command_help)
-    bot.send_message(message.chat.id, "Выбирай купить и покупай ...", reply_markup=user_markup)
+    bot.send_message(message.chat.id, u"Выбирай купить и покупай ...", reply_markup=user_markup)
 
 # stop
 @bot.message_handler(commands=['stop'])
 def handle_text(message):
     hide_markup = telebot.types.ReplyKeyboardRemove(True);
-    bot.send_message(message.chat.id,"Пока ...", reply_markup=hide_markup)
+    bot.send_message(message.chat.id, u"Пока ...", reply_markup=hide_markup)
 
 # help
 @bot.message_handler(commands=['help'])
 def handle_text(message):
-        bot.send_message(message.chat.id,"Выбери опцию покупка ...")
+        bot.send_message(message.chat.id,u"Выбери опцию покупка ...")
 
 # chat message
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     global i
+    global order
     step = 0
     if message.text == const.command_rules:
-        bot.send_message(message.chat.id, "Выбираем, оплачиваем, получаем ...")
+        bot.send_message(message.chat.id, u"Выбираем, оплачиваем, получаем ...")
         log(message, u"правила просты ...")
     elif message.text == const.command_review:
-        bot.send_message(message.chat.id, "Все елочки лучшего качества ...")
+        bot.send_message(message.chat.id, u"Все елочки лучшего качества ...")
         log(message, u"отзывы шикарны ...")
     elif message.text == const.command_buy:
         order.append(str(message.from_user.id))
@@ -85,42 +86,36 @@ def handle_text(message):
         i += 1
         log(message, u"оформление заказа")
     elif message.text == "!" and str(message.from_user.id) == id.boss:
-        bot.send_message(message.chat.id, "hi, master")
+        bot.send_message(message.chat.id, u"hi, master")
         log(message, u"виват ...")
     else:
-        bot.send_message(message.chat.id, "прочитай раздел помощь ...")
-        answer = "прочитай раздел помощь ..."
-#        log(message, answer)
+        bot.send_message(message.chat.id, u"прочитай раздел помощь ...")
+        log(message, u"раздел помощь ...")
     if step == 1:
         user_parkup = telebot.types.ReplyKeyboardMarkup(True, False)
-        # remove
-        user_parkup.row('/start', '/stop')
-        # /remove
         user_parkup.row(const.command_reg[8], const.command_reg[1], const.command_reg[2])
         user_parkup.row(const.command_reg[7], const.command_reg[0], const.command_reg[3])
         user_parkup.row(const.command_reg[5], const.command_reg[6], const.command_reg[4])
         user_parkup.row(const.command_review, const.command_rules, const.command_help)
-        bot.send_message(message.chat.id, "Продажа осуществляется в Москве.\nВыбирай район расположения базара ...",
+        bot.send_message(message.chat.id, u"Продажа осуществляется в Москве.\nВыбирай район расположения базара ...",
                          reply_markup=user_parkup)
     elif step == 2:
         user_parkup = telebot.types.ReplyKeyboardMarkup(True, False)
-        # remove
-        user_parkup.row('/start', '/stop')
-        # /remove
         user_parkup.row(const.command_goods[0], const.command_goods[1])
         user_parkup.row(const.command_review, const.command_rules, const.command_help)
-        bot.send_message(message.chat.id, "Выбирай купить и покупай ...", reply_markup=user_parkup)
+        bot.send_message(message.chat.id, u"Выбирай купить и покупай ...", reply_markup=user_parkup)
     elif step == 3:
         user_parkup = telebot.types.ReplyKeyboardMarkup(True, False)
-        # remove
-        user_parkup.row('/start', '/stop')
-        # /remove
+
         user_parkup.row(const.command_add, const.command_checkout)
         user_parkup.row(const.command_review, const.command_rules, const.command_help)
-        bot.send_message(message.chat.id, "Добавляй или оформляй ...", reply_markup=user_parkup)
+        bot.send_message(message.chat.id, u"Добавляй или оформляй ...", reply_markup=user_parkup)
     elif step == 4:
         bot.send_message(message.chat.id,u", ".join(order))
-    else:
-        bot.send_message(message.chat.id, "прочитай раздел помощь ...")
+        hide_markup = telebot.types.ReplyKeyboardRemove(True);
+        bot.send_message(message.chat.id, u"Для повторной покупки набери /start ...", reply_markup=hide_markup)
+        order = []
+#    else:
+#        bot.send_message(message.chat.id, u"прочитай раздел помощь ...")
 
 bot.polling(none_stop=True, interval=0)
